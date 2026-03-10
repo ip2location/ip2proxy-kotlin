ead1295f16c7540247f13563c58bdd40647180e8
IP2Location <support@ip2location.com>
2026-03-10 14:06:19 +0800

    Updated sample code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,11 +4,11 @@
 # -- Project information
 
 project = 'IP2Proxy Kotlin'
-copyright = '2025, IP2Location'
+copyright = '2026, IP2Location'
 author = 'IP2Location'
 
-release = '0.1.0'
-version = '0.1.0'
+release = '3.5.0'
+version = '3.5.0'
 
 # -- General configuration
 
@@ -57,4 +57,4 @@ html_favicon = 'images/favicon.ico'
 
 html_title = "IP2Proxy Kotlin"
 
-# html_baseurl = "https://ip2proxy-java.readthedocs.io/en/latest/"
+# html_baseurl = "https://ip2proxy-kotlin.readthedocs.io/en/latest/"
